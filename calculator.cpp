6afe371e20a80e78ef5853524e28684ce84ab3f1
Achilike Chibuikem <achilikechibuikem123@gmail.com>
2026-01-29 15:35:34 +0100

    Functioning now

--- calculator.cpp
+++ calculator.cpp
@@ -3,6 +3,27 @@ using namespace std;
 
 float calc(float a, float b, int op)
 {
+    if (op == 1)
+        return a + b;
+    else if (op == 2)
+        return a - b;
+    else if (op == 3)
+        return a * b;
+    else if (op == 4)
+    {
+        if (b != 0)
+            return a / b;
+        else
+        {
+            cout << "Error: Division by zero!" << endl;
+            return 0;
+        }
+    }
+    else
+    {
+        cout << "Invalid operation!" << endl;
+        return 0;
+    }
 }
 
 int main()
@@ -15,12 +36,15 @@ int main()
     cout << "Enter first num: ";
     cin >> num1;
 
-    cout << "Enter second nunber: ";
+    cout << "Enter second number: ";
     cin >> num2;
 
     cout << "Enter operation you wish to perform." << endl;
     cout << "1/2/3/4 = Add/Subtract/Multiply/Divide: ";
     cin >> operation;
+
+    float result = calc(num1, num2, operation);
+    cout << "The answer is: " << result << endl;
 
     return 0;
 }
