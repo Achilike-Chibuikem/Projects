47aadeacda366453a69920fc4e53c0be12fd7edc
Achilike Chibuikem <achilikechibuikem123@gmail.com>
2026-01-29 14:32:58 +0100

    Declared the "calc" function.

--- calculator.cpp
+++ calculator.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 using namespace std;
 
+float calc(float a, float b, int op)
+{
+}
+
 int main()
 {
     float num1, num2;
-    char operation;
+    int operation;
+
+    cout << "This is a simple calculator." << endl;
+
+    cout << "Enter first num: ";
+    cin >> num1;
+
+    cout << "Enter second nunber: ";
+    cin >> num2;
+
+    cout << "Enter operation you wish to perform." << endl;
+    cout << "1/2/3/4 = Add/Subtract/Multiply/Divide: ";
+    cin >> operation;
+
+    return 0;
 }
